3b45ef3747206674636ee0d3bb78eaaa86d6cc11
ZakirovaVera <zakirovavk@gmail.com>
2022-08-08 15:24:05 +0500

    массив поиск индекса

---
+++ example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 12, 56, 5, 98, 10, 63, 7, 98, 102 };
+
+int n = array.Length;
+int find = 98;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index +1;
+    index++;
+}
